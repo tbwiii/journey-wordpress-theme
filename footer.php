c1e2dc1fa2f3f035dc41aebd4ad5dfa01c38718a
Ted Waller <ted@tbwiii.com>
2014-02-11 18:30:38 -0500

    updated footer styles

--- footer.php
+++ footer.php
@@ -3,15 +3,17 @@
 	</div> <!-- End master-wrap -->
 
 	<footer>
-
-		<p class="copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
-
-		<div class="footer-widgets">
-			<?php dynamic_sidebar('footer'); ?>
+		<div class="copyright">
+			<p class="footer-wrap">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
 		</div>
+		<div class="footer-wrap">
+			<div class="footer-widgets">
+				<?php dynamic_sidebar('footer'); ?>
+			</div>
 
-		<?php if ( has_nav_menu( 'footer' ) ) wp_nav_menu( array('theme_location' => 'footer', 'container' => 'nav', 'container_id' => '', 'container_class' => 'footer-menu', 'menu_class' => 'footer-menu-ul', 'depth' => 1) ); ?>
-		<?php wp_footer(); ?>
+			<?php if ( has_nav_menu( 'footer' ) ) wp_nav_menu( array('theme_location' => 'footer', 'container' => 'nav', 'container_id' => '', 'container_class' => 'footer-menu', 'menu_class' => 'footer-menu-ul', 'depth' => 1) ); ?>
+			<?php wp_footer(); ?>
+		</div>
 	</footer>
 
 </body>
