f4c7e243267aa7c1eeaf13eb10680d6cc5c6226c
Ted Waller <ted@tbwiii.com>
2014-07-19 09:47:15 -0400

    update from site

--- header.php
+++ header.php
@@ -35,6 +35,16 @@
 	<title><?php wp_title('&lsaquo;', true, 'right'); ?></title>
 
 	<link href="<?php bloginfo('stylesheet_directory'); ?>/style.css" rel="stylesheet" type="text/css" />
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-26645934-1', 'thejourneyhiram.org');
+  ga('send', 'pageview');
+
+</script>
 
 	<?php wp_head(); ?>
 </head>
